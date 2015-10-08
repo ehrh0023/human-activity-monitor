b1bfc62ced4788f5cf0360dfa1506fe0372a7687
Lord Dennis <ehrh0023@umn.edu>
2015-10-08 14:58:25 -0500

    Fixed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "Object.h"
 
 using namespace cv;
 using namespace std;
@@ -14,12 +13,9 @@ using namespace std;
 int thresh = 80;
 const int max_thresh = 255;
 RNG rng(12345);
-
-vector<Object> objects;
-
 int main(int, char**)
 {
-	VideoCapture cap("c:/Users/Dennis/Desktop/box.mp4"); // open the default camera
+	VideoCapture cap(0); // open the default camera
 	if(!cap.isOpened())  // check if we succeeded
 		return -1;
 
@@ -39,53 +35,24 @@ int main(int, char**)
 		resize(frame, frame, Size(360, 240));
 		cvtColor(frame, frame_gray, CV_BGR2GRAY);
 		//blur(frame_gray, frame_gray, Size(3, 3));
-
 		GaussianBlur(frame_gray, frame_gray, Size(15, 15), 0);
-		
 
 		Canny(frame_gray, canny_output, thresh, thresh * 2, 3);
 		/// Find contours
 		findContours(canny_output, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
 		/// Get the moments
-		vector<Object> newObjects(contours.size());
+		vector<Moments> mu(contours.size());
 		for (size_t i = 0; i < contours.size(); i++)
 		{
-			Moments mu = moments(contours[i], false);
-			newObjects[i].moments = mu;
-			Point mc = Point2f(static_cast<float>(mu.m10 / mu.m00), static_cast<float>(mu.m01 / mu.m00));
-			newObjects[i].center = mc;
-
-			float area = 0.0f;
-			const float k_inv3 = 1.0f / 3.0f;
-			for (int j = 0; j < contours[i].size(); j++)
-			{
-				// Triangle vertices, third vertex implied as (0, 0)
-				Point2f p1(contours[i][j]);
-				int j2 = j + 1 < contours[i].size() ? j + 1 : 0;
-				Point2f p2(contours[i][j2]);
-
-				float D = p1.cross(p2);
-				float triangleArea = 0.5f * D;
-
-				area += triangleArea;
-			}
-			newObjects[i].mass = area;
+			mu[i] = moments(contours[i], false);
 		}
 
-		for (int i = 0; i < objects.size(); i++)
+		///  Get the mass centers:
+		vector<Point2f> mc(contours.size());
+		for (size_t i = 0; i < contours.size(); i++)
 		{
-			for (int j = 0; j < newObjects.size(); j++)
-			{
-				if (objects[i].compare(newObjects[j]))
-				{
-					objects[i].center = newObjects[j].center;
-					objects[i].mass = newObjects[j].mass;
-					objects[i].moments = newObjects[j].moments;
-					objects[i].contour = newObjects[j].contour;
-					break;
-				}
-			}
+			mc[i] = Point2f(static_cast<float>(mu[i].m10 / mu[i].m00), static_cast<float>(mu[i].m01 / mu[i].m00));
 		}
 
 		/// Draw contours
@@ -94,7 +61,7 @@ int main(int, char**)
 		{
 			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 			drawContours(drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point());
-			circle(drawing, newObjects[i].center, 4, newObjects[i].color, -1, 8, 0);
+			circle(drawing, mc[i], 4, color, -1, 8, 0);
 		}
 
 
