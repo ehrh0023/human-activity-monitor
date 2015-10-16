8af876644af1f2b70f82c46c60a2b154e0224183
Lord Dennis <ehrh0023@umn.edu>
2015-10-16 12:33:18 -0500

    Added Color Detection to FaceDetectTest

--- src/prototype/FaceDetectTest.cpp
+++ src/prototype/FaceDetectTest.cpp
@@ -26,7 +26,8 @@ int main(int, char**)
 			break;
 		}
 
-		Mat gray;
+		Mat gray, HSV;
+		cvtColor(img, HSV, COLOR_BGR2HSV);
 		cvtColor(img, gray, COLOR_BGR2GRAY);
 
 		// Find the faces
@@ -37,6 +38,43 @@ int main(int, char**)
 		for (Rect point : faces)
 		{
 			rectangle(img, point, Scalar(255, 0, 0), 2);
+
+			int minY = point.height * 0.25 + point.y;
+			int minX = point.width  * 0.25 + point.x;
+			int maxY = point.height * 0.75 + point.y;
+			int maxX = point.width  * 0.75 + point.x;
+
+			unsigned char minH = 255, minS = 255, minV = 255;
+			unsigned char maxH = 0,   maxS = 0,   maxV = 0;
+			for (int y = minY; y < maxY; y++)
+			{
+				for (int x = minX; x < maxX; x++)
+				{
+					cv::Vec3b pixel = HSV.at<Vec3b>(y, x);
+
+					// H = pixel[0]
+					if (pixel[0] < minH)
+						minH = pixel[0];
+					if (pixel[0] > maxH)
+						maxH = pixel[0];
+
+					// S = pixel[1]
+					if (pixel[1] < minS)
+						minS = pixel[1];
+					if (pixel[1] > maxS)
+						maxS = pixel[1];
+
+					// V = pixel[2]
+					if (pixel[2] < minV)
+						minV = pixel[2];
+					if (pixel[2] > maxV)
+						maxV = pixel[2];
+				}
+			}
+
+			std::cout << "H: " << (int)minH << " - " << (int)maxH << std::endl
+				<< "S: " << (int)minS << " - " << (int)maxS << std::endl
+				<< "V: " << (int)minV << " - " << (int)maxV << std::endl;
 		}
 
 		imshow("img", img);//wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
