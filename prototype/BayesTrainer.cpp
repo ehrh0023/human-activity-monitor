810cd5c11d62df3326be9e834f3673dcb3c9dd48
forne058 <forne058@umn.edu>
2015-11-05 14:06:24 -0600

    Fixed Bayes Trainer to work for image 4 and 7.
    Fixed Bayes Trainer to work for image 4 and 7 for now it is a wrapper
    that works by cropping the image first.
    image 3 is skipped because no BW image is provided for image 3

--- prototype/BayesTrainer.cpp
+++ prototype/BayesTrainer.cpp
@@ -9,21 +9,30 @@ int main(int, char**)
 
 	for (int i = 1; i <= 11; ++i)
 	{
-		if (i == 3 || i == 4 || i == 7)
+		if (i == 3)
 			continue;
 
 		const std::string imgName = "C:/Users/brian_000/Desktop/Pictures/Unprocessed/pic";
 		const std::string bwName = "C:/Users/brian_000/Desktop/Pictures/BW/picbw";
-        Rect crop(10, 10, 1280,720);
+        Rect crop(10, 10, 400, 300);      // image 4
+        Rect crop2(10, 10, 1150, 1150);   // image 7
 		// read training image
 		std::string normfile = imgName + std::to_string(i) + ".jpg";
 		cv::Mat image = imread(normfile);
 		
-		//if (i == 7 || i == 8)
-		//Mat image = cimage(crop);
-
-		resize(image, image, Size(2000, 2000));
-
+		if (i == 4)
+		{ 
+		   image = image(crop);
+		   resize(image,image,Size(2000,2000));
+	    }
+	    else if (i == 7)
+        {
+	 	   image = image(crop2);
+	 	   resize(image,image,Size(2000,2000)); 	   
+	    }
+        else
+		   resize(image, image, Size(2000,2000));
+		
 		cvtColor(image, image, CV_RGB2GRAY);
 		image.convertTo(image, CV_32S); // train expects a matrix of integers
 		image = image.reshape(0, image.rows*image.cols); // little trick number 2 convert your width x height, N channel image into a witdth*height row matrix by N columns, as each pixel should be considere as a training sample.
@@ -31,11 +40,19 @@ int main(int, char**)
 		// read mask image
 		std::string maskfile = bwName + std::to_string(i) + ".jpg";
 		cv::Mat mask = imread(maskfile);
-		//if (i == 7 || i == 8)
-		//Mat mask = cmask(crop);
-
-		resize(mask, mask, Size(2000, 2000));
-
+		if (i == 4)
+		{
+		   mask = mask(crop);
+		   resize(mask,mask, Size(2000,2000));
+	    }
+	    else if (i == 7)
+	    {
+	  	   mask = mask(crop2);
+	  	   resize(mask,mask,Size(2000,2000)); 	   
+	    }
+		else
+		   resize(mask, mask, Size(2000,2000));
+        
 		cvtColor(mask, mask, CV_RGB2GRAY);
 		mask.convertTo(mask, CV_32S); // train expects a matrix of integers
 		mask = mask.reshape(0, image.rows*image.cols); // little trick number 2 convert your width x height, N channel image into a witdth*height row matrix by N columns, as each pixel should be considere as a training sample.
