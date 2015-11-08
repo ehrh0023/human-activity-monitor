2d34bb6f6d1b1357282952ad0393c3261bdf1a68
forne058 <forne058@umn.edu>
2015-11-08 11:06:16 -0600

    Added Chronos Python Script
    Developed Chronos Python script for extracting raw accelerometer data.
    Compiled with VS 15. Can be run by opening command prompt and typing in
    the just the location of the python source file. It will then create an
    ACCEL.csv file in the folder where the source file is contianed. This
    ACCEL.csv file contains the acceleration data captured from the Chronos
    watch. Only the data where a single coordinate (x,y or z) is greater
    than 0 will be recorded. If all are zero nothing will be saved. Each
    record is time stamped in seconds for now.

---
+++ Chronos/Chronos_ACCDATA/Chronos_ACCDATA.py
@@ -0,0 +1,33 @@
+﻿#Get acceleration data from Chronos watch.
+import serial
+import time
+import array
+import csv
+ 
+def startAccessPoint():
+    return array.array('B', [0xFF, 0x07, 0x03]).tostring()
+ 
+def accDataRequest():
+    return array.array('B', [0xFF, 0x08, 0x07, 0x00, 0x00, 0x00, 0x00]).tostring()
+ 
+#Open COM port 6 
+#argments are 5 (COM6), 115200 (bit rate), and timeout is set so
+ser = serial.Serial(3,115200,timeout=1)
+ 
+#Start access point
+ser.write(startAccessPoint())
+
+while True:
+    #Send request for acceleration data
+    ser.write(accDataRequest())
+    accel = ser.read(7)
+    
+    #if ord(accel[0]) != 0 and ord(accel[1]) != 0 and ord(accel[2]) != 0:
+    print("x: " + str(accel[0]) + "y: " + str(accel[1]) + "z: " + str(accel[2])) 
+    #Save Accel data to CSV file if coordinate is not 0
+    with open('Accel.csv', 'a') as csvfile:
+       fieldnames = ['x', 'y', 'z', 'Time']
+       writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+       if accel[0] != 0 or accel[1] != 0 or accel[2] != 0:
+        writer.writerow({'x': accel[0], 'y': accel[1], 'z': accel[2], 'Time': time.strftime("%S")})
+ser.close()
