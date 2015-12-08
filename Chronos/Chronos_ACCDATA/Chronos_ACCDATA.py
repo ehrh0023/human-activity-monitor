ca7e0c81391bb7db080a888e9e4fcb40a85e81c7
forne058 <forne058@umn.edu>
2015-12-07 22:27:25 -0600

    Updated to determine sample rate of Chronos watch for time extraction.

--- Chronos/Chronos_ACCDATA/Chronos_ACCDATA.py
+++ Chronos/Chronos_ACCDATA/Chronos_ACCDATA.py
@@ -1,8 +1,10 @@
 ﻿#Get acceleration data from Chronos watch.
 import serial
-import time
+#import time
 import array
 import csv
+import datetime
+#import time
  
 def startAccessPoint():
     return array.array('B', [0xFF, 0x07, 0x03]).tostring()
@@ -10,7 +12,6 @@ def startAccessPoint():
 def accDataRequest():
     return array.array('B', [0xFF, 0x08, 0x07, 0x00, 0x00, 0x00, 0x00]).tostring()
   
-import datetime
 
 
 #Open COM port 6 
@@ -21,22 +22,20 @@ ser = serial.Serial(3,115200,timeout=1)
 ser.write(startAccessPoint())
 
 with open('C:/Users/brian_000/Documents/GitHub/human-activity-monitor/Chronos/Chronos_ACCDATA/Accel.csv', 'a') as csvfile:
-       fieldnames = ['x', 'y', 'z', 'Hours', 'Minutes', 'Seconds', 'MicroSeconds']
+       fieldnames = ['x', 'y', 'z', 'Seconds', 'Microseconds']
        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
        writer.writeheader()
-
+       
+now = datetime.datetime.now()
 while True:
-    now = datetime.datetime.now()
     #Send request for acceleration data
     ser.write(accDataRequest())
     accel = ser.read(7)
-    
-    #if ord(accel[0]) != 0 and ord(accel[1]) != 0 and ord(accel[2]) != 0:
-    #print("x: " + str(accel[0]) + "y: " + str(accel[1]) + "z: " + str(accel[2])) 
+    duration = datetime.datetime.now() - now
+    print("x: " + str(ord(accel[0])) + "y: " + str(ord(accel[1])) + "z: " + str(ord(accel[2]))) 
     #Save Accel data to CSV file if coordinate is not 0
     with open('C:/Users/brian_000/Documents/GitHub/human-activity-monitor/Chronos/Chronos_ACCDATA/Accel.csv', 'a') as csvfile:
-       fieldnames = ['x', 'y', 'z', 'Hours', 'Minutes', 'Seconds', 'MicroSeconds']
+       fieldnames = ['x', 'y', 'z', 'Seconds', 'Microseconds']
        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-       if (accel[0] != 0 and accel[1] != 0 and accel[2] != 0) or accel[0] = 255:
-          writer.writerow({'x': accel[0], 'y': accel[1], 'z': accel[2], 'Hours': time.strftime("%H"), 'Minutes': time.strftime("%M"), 'Seconds': time.strftime("%S"), 'MicroSeconds': str(now.microsecond)})
+       writer.writerow({'x': str(ord(accel[0])), 'y': str(ord(accel[1])), 'z': str(ord(accel[2])),'Seconds': duration.seconds, 'Microseconds': duration.microseconds})
 ser.close()
